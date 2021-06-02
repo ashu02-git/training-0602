a7a1d59db6c17cd3a3c2eeecd175a53ed6d0c92f
ashu02-git <shuhei.abe1107@gmail.com>
2021-06-02 14:02:23 +0900

    wish名検索機能実装

--- wish-list/routes/wishlist.js
+++ wish-list/routes/wishlist.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var sqlight3 = require('sqlite3');
 const db = new sqlight3.Database('wishlist.db');
 
-
+/* GET wishlist */
 router.get('/',(req, res, next)=> {
     db.serialize(()=>{
       db.all("select * from wishlist",(err,rows)=>{
@@ -17,4 +17,21 @@ router.get('/',(req, res, next)=> {
       });
     });
 });
+
+/* search */
+router.get('/search',(req, res, next)=> {
+  const keyword = req.query
+  db.serialize(()=>{
+    db.all(`select * from wishlist where wish LIKE "%${keyword.wish}%"`,(err,rows)=>{
+      if(!err){
+        var data={
+          title: "Wish List",
+          content: rows
+        };
+        res.render('wishlist', data);
+      }
+    });
+  });
+});
+
 module.exports = router;
